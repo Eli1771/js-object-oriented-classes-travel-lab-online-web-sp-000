8dc7ad67f553c77f38ee6fe5c3a0324f2c97815d
elijahmm17 <elijahmm17@gmail.com>
2019-09-29 02:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+class Driver {
+  constructor(name, dayJoined) {
+    this.name = name;
+    this.dayJoined = dayJoined;
+  }
+  
+  startDate() {
+    
+  }
+  yearsExperienceFromBeginningOf() {
+    
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled() {
+    
+  }
+  estimatedTime() {
+    
+  }
+}
